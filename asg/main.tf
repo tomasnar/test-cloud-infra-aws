7b03f2bf39dcf9f0c3eee42c3cb03152b2b3dca7
Tomas Narbutaitis <tomasnar@gmail.com>
2025-10-26 13:36:56 +0800

    ssh keyremove

--- asg/main.tf
+++ asg/main.tf
@@ -8,11 +8,6 @@ provider "aws" {
 
 data "aws_availability_zones" "allaz" {}
 
-resource "aws_key_pair" "keypair" {
-  key_name   = "${var.name}-keypair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAmn0Fz/9Y50QobhLnOP1Y5Sa+FtyUutH9X8wK+o7vfgEKN6KUO4MfleNvZZuIT2iy2EJLSUgBDz8ck1c7tJkC/zzLZyv/Y8T8SI1G3DyMTOPPwFd6Al9shN0Lc1Sh/BFricY7RjoUV8bjQTev9t4zaUFu/+AS/o/ZardNHKgavGW9e3sMnMLweTDPWSwT8EUPFZLdapPXvV3kbF8j4P9cAjLFhxlKy1COe+RkivjcdzlG9gVWq8yvlrSFyUTl40BCYlWbEdRhBhR8ppnP/T3KXOfkH6PJ8P2GLTpTiZhrTD/X4CxzkkNsVoEUllFGU8LXcKI+TdLPEGjMbUPr7Jr0x1Y4IZ4qzzHW3uZ1i6LedJgLtKyJla0v+rGYbR9vvvjYfln3KDazNLUnS82B/ONboADjK3Ts7br/E2+kJQ7GhrJnxQbywdh2ftcQz4SaWNiq/Zb5TiJxjDdizJf7j59kDUHB+fCOGSEF2y5rLBNsh86mbZLIwfyNuXQHS89yStV/qgcize3DviMJbNr2i2EkseLyV8upcD6+UtLoa5ZcqsYLZCQf6FuufBusoFmX+RO5EazZqVVrRcaoxwzCVM9+a2FcdW8LX4B8Il0DHScuexRSQUxMMRw+w6SYfDhA1SXol0dE7RF5zqk+vPqLfSNyZu8pcop/lWhiHFJP07WFVu0= rsa-key-20210730"
-}
-
 module "nlb" {
   source = "terraform-aws-modules/alb/aws"
 
@@ -25,12 +20,6 @@ module "nlb" {
 
   # Security Group
   security_group_ingress_rules = {
-    ssh_rule = {
-      from_port   = 22
-      to_port     = 22
-      description = "SSH traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
     web_rule = {
       from_port   = 80
       to_port     = 80
@@ -58,29 +47,12 @@ module "nlb" {
         target_group_key = "ex-target-one"
       }
     }
-
-    ex-two = {
-      port     = 22
-      protocol = "TCP"
-      forward = {
-        target_group_key = "ex-target-two"
-      }
-    }
   }
   target_groups = {
     ex-target-one = {
       name_prefix            = "web-"
-      protocol               = "TCP_UDP"
+      protocol               = "TCP"
       port                   = 80
-      connection_termination = true
-      preserve_client_ip     = true
-      create_attachment      = false
-    }
-
-    ex-target-two = {
-      name_prefix = "ssh-"
-      protocol    = "TCP"
-      port        = 22
       connection_termination = true
       preserve_client_ip     = true
       create_attachment      = false
